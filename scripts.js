c9b1de0498e833c7c8430c67bd6c5ef39fda142e
unknown <umeshluffy@gmail.com>
2024-02-07 02:59:25 -0800

    Adding the  prcts

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var portfolioLink = document.getElementById("portfolio-link");
+    if (portfolioLink) {
+        portfolioLink.innerText = "Photography Gallery";
+    }
+});
